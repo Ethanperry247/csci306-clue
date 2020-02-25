0ee441f8cab6b0564314ea200f588d9ec3132bc4
Cpanocc <55562352+CPanocc@users.noreply.github.com>
2020-02-25 13:27:49 -0700

    badconfig exception skeleton and enum type added

---
+++ C12A2_Perry_Pan/Part II/src/clueGame/BadConfigFormatException.java
@@ -0,0 +1,14 @@
+package clueGame;
+
+public class BadConfigFormatException extends Exception {
+	
+	public BadConfigFormatException() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "BadConfigFormatException []";
+	}
+	
+}
