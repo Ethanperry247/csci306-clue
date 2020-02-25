de6f1d66e7a27639da6d12d697ce7080cc7aa44a
Cpanocc <55562352+CPanocc@users.noreply.github.com>
2020-02-25 13:48:09 -0700

    badconfig exception extended

--- C12A2_Perry_Pan/Part II/src/clueGame/BadConfigFormatException.java
+++ C12A2_Perry_Pan/Part II/src/clueGame/BadConfigFormatException.java
@@ -1,9 +1,32 @@
 package clueGame;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class BadConfigFormatException extends Exception {
 	
-	public BadConfigFormatException() {
-		super();
+	public BadConfigFormatException() throws FileNotFoundException {
+		super("Error loading config file from formatting incompatibility");
+		try {
+	    	PrintWriter out = new PrintWriter("logfile.txt");
+	    	out.println("Error logged: formatting incompatibility");
+	        out.close();
+	        
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	}
+	
+	public BadConfigFormatException(String s) throws FileNotFoundException {
+		super(s);
+		try {
+	    	PrintWriter out = new PrintWriter("logfile.txt");
+	    	out.println("Error logged: " + s);
+	        out.close();
+	        
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
 	}
 
 	@Override
