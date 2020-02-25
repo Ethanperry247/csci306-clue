c4d4858e91b29d7693061c06e9e117e7b99e61a6
Ethanperry247 <eperry1@gmail.com>
2020-02-25 13:11:46 -0700

    Added new clueGame package and class to that package.

---
+++ C12A2_Perry_Pan/Part II/src/clueGame/BoardCell.java
@@ -0,0 +1,20 @@
+package clueGame;
+
+public class BoardCell {
+	
+	private int row;
+	private int column;
+
+	public BoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardCell [row=" + row + ", column=" + column + "]";
+	}
+	
+	
+
+}
