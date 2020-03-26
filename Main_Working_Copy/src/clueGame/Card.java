0cac57f17d5c28442570122c317cbd25c107a222
Cpanocc <55562352+CPanocc@users.noreply.github.com>
2020-03-25 22:27:56 -0600

    added createDeck in Card.java that creates all required cards in the game

--- Main_Working_Copy/src/clueGame/Card.java
+++ Main_Working_Copy/src/clueGame/Card.java
@@ -1,5 +1,7 @@
 package clueGame;
 
+import java.util.HashSet;
+
 public class Card {
 	private String cardName;
 	private CardType type;
@@ -7,6 +9,37 @@ public class Card {
 	public Card(String cardName, CardType type) {
 		this.cardName = cardName;
 		this.type = type;
+	}
+	
+	public void createDeck() {
+		
+		// 9 room cards
+		Card Greenhouse = new Card("Greenhouse", CardType.ROOM);
+		Card Dungeon = new Card("Dungeon", CardType.ROOM);
+		Card Studio = new Card("Studio", CardType.ROOM);
+		Card Cellar = new Card("Cellar", CardType.ROOM);
+		Card Atrium = new Card("Atrium", CardType.ROOM);
+		Card Veranda = new Card("Veranda", CardType.ROOM);
+		Card Lounge = new Card("Lounge", CardType.ROOM);
+		Card Parlor = new Card("Parlor", CardType.ROOM);
+		Card Office = new Card("Office", CardType.ROOM);
+		
+		// 6 people cards
+		Card Scarlett = new Card("Scarlett", CardType.PERSON);
+		Card Green = new Card("Green", CardType.PERSON);
+		Card Mustard = new Card("Mustard", CardType.PERSON);
+		Card Plum = new Card("Plum", CardType.PERSON);
+		Card Peacock = new Card("Peacock", CardType.PERSON);
+		Card White = new Card("White", CardType.PERSON);
+		
+		// 6 weapon cards
+		Card Candlestick = new Card("Candlestick", CardType.WEAPON);
+		Card Dagger = new Card("Dagger", CardType.WEAPON);
+		Card Pipe = new Card("Pipe", CardType.WEAPON);
+		Card Revolver = new Card("Revolver", CardType.WEAPON);
+		Card Rope = new Card("Rope", CardType.WEAPON);
+		Card Wrench = new Card("Wrench", CardType.WEAPON);
+		
 	}
 	
 	// to be implemeted
