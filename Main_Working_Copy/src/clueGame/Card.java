676cd05a8f9c468aa65bf6c0d5596e1e007ea28b
Cpanocc <55562352+CPanocc@users.noreply.github.com>
2020-03-28 22:16:40 -0600

    modified tests so that @BeforeClass is properly implemented along with final thorough documentation for Player Part I

--- Main_Working_Copy/src/clueGame/Card.java
+++ Main_Working_Copy/src/clueGame/Card.java
@@ -12,61 +12,6 @@ public class Card {
 		this.type = type;
 	}
 	
-	public void createDeck(Queue<Card> rooms, Queue<Card> people, Queue<Card> weapons) {	// cards will be split into three queues (decks) for each card type
-		
-		// 9 room cards
-		rooms.add(new Card("Greenhouse", CardType.ROOM));
-		rooms.add(new Card("Dungeon", CardType.ROOM));
-		rooms.add(new Card("Studio", CardType.ROOM));
-		rooms.add(new Card("Cellar", CardType.ROOM));
-		rooms.add(new Card("Atrium", CardType.ROOM));
-		rooms.add(new Card("Veranda", CardType.ROOM));
-		rooms.add(new Card("Lounge", CardType.ROOM));
-		rooms.add(new Card("Parlor", CardType.ROOM));
-		rooms.add(new Card("Office", CardType.ROOM));
-		
-		// 6 people cards
-		people.add(new Card("Miss Scarlett", CardType.PERSON));
-		people.add(new Card("Rev. Green", CardType.PERSON));
-		people.add(new Card("Colonel Mustard", CardType.PERSON));
-		people.add(new Card("Professor Plum", CardType.PERSON));
-		people.add(new Card("Mrs. Peacock", CardType.PERSON));
-		people.add(new Card("Mrs. White", CardType.PERSON));
-		
-		// 6 weapon cards
-		weapons.add(new Card("Candlestick", CardType.WEAPON));
-		weapons.add(new Card("Dagger", CardType.WEAPON));
-		weapons.add(new Card("Pipe", CardType.WEAPON));
-		weapons.add(new Card("Revolver", CardType.WEAPON));
-		weapons.add(new Card("Rope", CardType.WEAPON));
-		weapons.add(new Card("Wrench", CardType.WEAPON));
-		
-	}
-	
-	public void dealCards(Queue<Card> rooms, Queue<Card> people, Queue<Card> weapons, ArrayList<Player> players) {	// another parameter is an array list of players in the game
-		
-		while (!rooms.isEmpty() || !people.isEmpty() || !weapons.isEmpty()) {	// will continue looping as long there is a non-empty deck, therefore all are cards dealt
-			
-			for (int i = 0; i < players.size(); i++) {	// iterates through players in the game
-				
-				// since each player gets dealt one card of each type at each iteration, the amount of cards each player has is roughly equal
-				// since the cards are stored in queues, the cards will be removed as we deal them so no card will be dealt twice
-				// each if statement will deal a card from one of the three decks if they have any cards left
-				if (!rooms.isEmpty()) {
-					players.get(i).getCards().add(rooms.remove());
-				}
-				
-				if (!people.isEmpty()) {
-					players.get(i).getCards().add(people.remove());
-				}
-				
-				if (!weapons.isEmpty()) {
-					players.get(i).getCards().add(weapons.remove());
-				}
-			}
-		}
-		
-	}
 	
 	// to be implemeted
 	public boolean equals() {
--- Main_Working_Copy/src/tests/gameSetupTests.java
+++ Main_Working_Copy/src/tests/gameSetupTests.java
@@ -13,35 +13,35 @@ import clueGame.CardType;
 
 public class gameSetupTests {
 	
-	
+	private static Board board;
 	
 	@BeforeClass
 	public static void setUp() {
 		
-		Board board = Board.getInstance();
+		board = Board.getInstance();
 		board.setConfigFiles("ClueBoardLayout.csv", "ClueRooms.txt", "CluePlayers.txt", "ClueWeapons.txt");
 		board.initialize();
 		board.loadConfigFiles();
+		board.dealDeck();
 		
 	}
 	
 	@Test
 	public void testLoadedPlayers() {
 		
-		Board board = Board.getInstance();
-		board.setConfigFiles("ClueBoardLayout.csv", "ClueRooms.txt", "CluePlayers.txt", "ClueWeapons.txt");
-		board.initialize();
-		board.loadConfigFiles();
+		// there are 7 loaded players, each player has 4 tests: if name, location (row and column), and color are all loaded correctly
 		
 		// Test the three possible human players.
-		assertTrue(board.getHumanPlayerNames().contains("Ms. Scarlett"));
-		assertEquals(0, board.getPlayer("Ms. Scarlett").getRow());
+		assertTrue(board.getHumanPlayerNames().contains("Ms. Scarlett"));	 // test if name has been loaded in correctly
+		assertEquals(0, board.getPlayer("Ms. Scarlett").getRow());			 // tests if location has been loaded in correctly (row and column)
 		assertEquals(3, board.getPlayer("Ms. Scarlett").getCol());
-		assertEquals(Color.RED, board.getPlayer("Ms. Scarlett").getColor());
+		assertEquals(Color.RED, board.getPlayer("Ms. Scarlett").getColor()); // tests if color has been loaded in correctly
+		
 		assertTrue(board.getHumanPlayerNames().contains("Mr. Green"));
 		assertEquals(0, board.getPlayer("Mr. Green").getRow());
 		assertEquals(9, board.getPlayer("Mr. Green").getCol());
 		assertEquals(Color.GREEN, board.getPlayer("Mr. Green").getColor());
+		
 		assertTrue(board.getHumanPlayerNames().contains("Col. Mustard"));
 		assertEquals(5, board.getPlayer("Col. Mustard").getRow());
 		assertEquals(19, board.getPlayer("Col. Mustard").getCol());
@@ -52,14 +52,17 @@ public class gameSetupTests {
 		assertEquals(13, board.getPlayer("Prof. Plum").getRow());
 		assertEquals(19, board.getPlayer("Prof. Plum").getCol());
 		assertEquals(Color.MAGENTA, board.getPlayer("Prof. Plum").getColor());
+		
 		assertTrue(board.getComputerPlayerNames().contains("Mrs. Peacock"));
 		assertEquals(19, board.getPlayer("Mrs. Peacock").getRow());
 		assertEquals(9, board.getPlayer("Mrs. Peacock").getCol());
 		assertEquals(Color.BLUE, board.getPlayer("Mrs. Peacock").getColor());
+		
 		assertTrue(board.getComputerPlayerNames().contains("Mrs. White"));
 		assertEquals(12, board.getPlayer("Mrs. White").getRow());
 		assertEquals(0, board.getPlayer("Mrs. White").getCol());
 		assertEquals(Color.WHITE, board.getPlayer("Mrs. White").getColor());
+		
 		assertTrue(board.getComputerPlayerNames().contains("Dr. Mr. Prof. Scott Strong"));
 		assertEquals(19, board.getPlayer("Dr. Mr. Prof. Scott Strong").getRow());
 		assertEquals(5, board.getPlayer("Dr. Mr. Prof. Scott Strong").getCol());
@@ -70,18 +73,15 @@ public class gameSetupTests {
 	@Test
 	public void testDeckOfCards() {
 		
-		Board board = Board.getInstance();
-		board.setConfigFiles("ClueBoardLayout.csv", "ClueRooms.txt", "CluePlayers.txt", "ClueWeapons.txt");
-		board.initialize();
-		board.loadConfigFiles();
-		board.dealDeck();
+		// tests for deck creation: size deck of 24 with 7 players, 6 weapons, and 9 rooms
 		
-		// Should be of size 24 with 7 players, 6 weapons, and 9 rooms.
-		assertEquals(24, board.getDeck().size());
-		assertEquals(7, board.getNumPlayers());
-		assertEquals(6, board.getNumWeapons());
-		assertEquals(11, board.getNumRooms());
-		assertEquals(board.getCard("Greenhouse").getType(), CardType.ROOM);
+		assertEquals(24, board.getDeck().size());	// tests if expected amount of total cards are created 
+		assertEquals(7, board.getNumPlayers());		// tests if expected amount of player cards are created 
+		assertEquals(6, board.getNumWeapons());		// tests if expected amount of weapons cards are created 
+		assertEquals(11, board.getNumRooms());		// tests if expected amount of room cards are created 
+		
+		// check if card types were loaded correctly
+		assertEquals(board.getCard("Greenhouse").getType(), CardType.ROOM);	
 		assertEquals(board.getCard("Dagger").getType(), CardType.WEAPON);
 		assertEquals(board.getCard("Mr. Green").getType(), CardType.PERSON);
 		
