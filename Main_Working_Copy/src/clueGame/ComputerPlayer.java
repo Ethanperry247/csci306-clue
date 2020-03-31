bb35788764fbe055a8bdd283592947e0e91e690b
Cpanocc <55562352+CPanocc@users.noreply.github.com>
2020-03-31 14:08:38 -0600

    added in pickLocation for ComputerPlayer, still needs to be intergrated into the gameplay for testing

--- Main_Working_Copy/src/clueGame/ComputerPlayer.java
+++ Main_Working_Copy/src/clueGame/ComputerPlayer.java
@@ -3,7 +3,10 @@
 package clueGame;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.Random;
 
 public class ComputerPlayer extends Player{
 
@@ -13,10 +16,26 @@ public class ComputerPlayer extends Player{
 	
 	// all methods below to be implemented
 	
-	public BoardCell pickLocation(Set<BoardCell> targets) {
-		BoardCell x = new BoardCell(0,0,"W");
-		return x;
+	public BoardCell pickLocation(Set<BoardCell> targets, BoardCell justVisited) {	// parameters are set of calculated target cells and the doorway cell of most recently visited room
+		ArrayList<BoardCell> rooms = new ArrayList<BoardCell>();	
+		
+		for (BoardCell cell : targets) {	// finds cells that are doorways (able to enter into those rooms)
+			if (cell.isDoorway()) {
+				rooms.add(cell);
+			}
+		}
+		
+		if (rooms.isEmpty() || (justVisited != null && rooms.contains(justVisited))) {	// if there is no rooms to go into or room just visited is an option...
+			rooms = new ArrayList<BoardCell>(targets);
+			Random rand = new Random();
+			int n = rand.nextInt(rooms.size());					// ...select an available target randomly
+			return rooms.get(n);
+		} else {
+			return rooms.get(0);								// else, room that was not just visited must be selected
+		}
+		
 	}
+	
 	
 	public void makeAccusation() {
 		
