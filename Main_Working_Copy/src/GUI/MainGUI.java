841f3b8c91769868ff068777e29566db2746c130
Ethanperry247 <eperry1@gmail.com>
2020-04-12 16:14:07 -0600

    Fixed an error with the opening message dialog. Now it displays all the correct information.

--- Main_Working_Copy/src/GUI/MainGUI.java
+++ Main_Working_Copy/src/GUI/MainGUI.java
@@ -42,7 +42,7 @@ public class MainGUI extends JFrame {
 	// Opening dialog will display the name of the human (first) player.
 	public void openingDialog() {
 		String message = "You are " + player.getName() + ", Press Next Player to begin play."; 
-		JOptionPane.showMessageDialog(null, message);
+		JOptionPane.showMessageDialog(this, message, "Welcome to Clue", JOptionPane.INFORMATION_MESSAGE);
 	}
 
 }
